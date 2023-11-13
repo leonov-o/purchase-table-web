1ae974f34cab9a69a83a91d0a63408ac026005e4
leonov <oleksandr.leonov.work@gmail.com>
2023-11-13 23:24:59 +0200

    Add few shared components

--- client/src/shared/ui/checkboxWithLabel/CheckboxWithLabel.tsx
+++ client/src/shared/ui/checkboxWithLabel/CheckboxWithLabel.tsx
@@ -1,15 +1,20 @@
 import {Checkbox} from "@/components/ui/checkbox.tsx";
 import {Label} from "@/components/ui/label.tsx";
+import React from "react";
 
 interface IInputWithLabelProps {
+    id?: string,
     label: string,
     description?: string,
+    checked: boolean,
+    onClick(e: React.MouseEvent<HTMLButtonElement>): void;
     className?: string
 }
-export const CheckboxWithLabel = ({label, description, className}: IInputWithLabelProps) => {
+
+export const CheckboxWithLabel = ({id, label, description, checked, onClick, className}: IInputWithLabelProps) => {
     return (
         <div className={"flex justify-start " + className}>
-            <Checkbox/>
+            <Checkbox id={id} checked={checked} onClick={onClick}/>
             <div className="grid leading-none gap-1.5 ml-1.5">
                 <Label>{label}</Label>
                 <p className="text-sm text-muted-foreground">
--- client/src/shared/ui/inputWithLabel/InputWithLabel.tsx
+++ client/src/shared/ui/inputWithLabel/InputWithLabel.tsx
@@ -1,17 +1,22 @@
 import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
+import {ChangeEvent} from "react";
 
 interface IInputWithLabelProps {
-    type: string,
+    type?: string,
     label: string,
     placeholder: string,
+    defaultValue?: string,
+    value: string,
+    onChange(e:ChangeEvent<HTMLInputElement>): void,
     className?: string
 }
-export const InputWithLabel = ({type, label, placeholder, className}: IInputWithLabelProps) => {
+
+export const InputWithLabel = ({type, label, placeholder, defaultValue, value, onChange, className}: IInputWithLabelProps) => {
     return (
         <div className={className}>
             <Label className="ml-1">{label}</Label>
-            <Input className="mt-2" type={type} placeholder={placeholder}/>
+            <Input className="mt-2" type={type} placeholder={placeholder} defaultValue={defaultValue} value={value} onChange={onChange}/>
         </div>
     );
 };
